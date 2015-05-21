51d68c7c408031b16034b39b6f9a9c3fe16cbf92
Everett Kropf <ehkropf@gmail.com>
2015-05-21 10:55:58 +0100

    Fix license statement.

--- skprod.m
+++ skprod.m
@@ -45,18 +45,18 @@ function wf = skprod(dv, qv, L)
 
 % Copyright Everett Kropf, 2015
 % 
-% skprime is free software: you can redistribute it and/or modify
+% skprod is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
-% skprime is distributed in the hope that it will be useful,
+% skprod is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 % 
 % You should have received a copy of the GNU General Public License
-% along with skprime.  If not, see <http://www.gnu.org/licenses/>.
+% along with skprod.  If not, see <http://www.gnu.org/licenses/>.
 
 if nargin < 2
     lmax = 4;
